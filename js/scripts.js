adf2b14fcedf40cfa0f62af758328c51f3ebe7be
Marqis Ramos <atair01@yahoo.com>
2019-06-14 12:40:45 -0700

    working on functional logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
+
   $("form#conV").submit(function(event){
   event.preventDefault();
+  var userInput = $("input#number").val();
+});
 
-var for3 = "I'm sorry, Dave. I'm afraid I can't do that."
-var for2 = "Boop"
-var for1 = "Beep"
+  var for3 = "I'm sorry, Dave. I'm afraid I can't do that."
+  var for2 = "Boop"
+  var for1 = "Beep"
+  var ary = [];
 
-   for 
+
+  for( var i = 0; i <= userInput; i++){
+    ary.push(i);
+  }
+  for(var i = 0; i < ary.length; i++){
+    if (ary[i].length > 1){
+      var
+    }
+  }
+});
