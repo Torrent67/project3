79ed482924bab8d0964ee000c82919157b048ef2
Marqis Ramos <atair01@yahoo.com>
2019-06-14 14:07:15 -0700

    got basic logic working, finilizing

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,30 @@ $(document).ready(function(){
   $("form#conV").submit(function(event){
   var userInput = $("input#number").val();
 
-  var for3 = "I'm sorry, Dave. I'm afraid I can't do that."
-  var for2 = "Boop"
-  var for1 = "Beep"
+  var for3 = "I'm sorry, Dave. I'm afraid I can't do that.";
+  var for2 = "Boop";
+  var for1 = "Beep";
   var ary = [];
   var spt = [];
 
 
   for( var i = 0; i <= userInput; i++){
     ary.push(i);
-  }
+    }
   for(var i = 0; i < ary.length; i++){
-    if (ary[i].length > 1){
-    spt = ary[i].split('');
-    }
-    if(spt.indexOf('1') >=0 && spt.indexOf('2') <0 && spt.indexOf('3')){
-      ary.splice(i,1,for1);
-    }
+    if (i > 9 ) {
+    spt = ary[i].toString().split('');
+  } else {
   }
-  alert(spt);
+    if(spt.indexOf('1') >=0 && spt.indexOf('2') <0 &&  spt.indexOf('3') <0){
+      ary.splice(i,1,for1);
+    } else if (spt.indexOf('2') >=0 && spt.indexOf('3') <0){
+      ary.splice(i,1,for2);
+    } else if (spt.indexOf('3') >=0){
+      ary.splice(i,1,for3);
+  }
+}
+  alert(ary);
   event.preventDefault();
 });
 });
