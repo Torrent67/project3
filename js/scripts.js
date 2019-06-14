c8674e6989ae7349de70cf7c937d2166bed523f2
Marqis Ramos <atair01@yahoo.com>
2019-06-14 13:26:57 -0700

    fixed certain html issues stioll working on logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 
   $("form#conV").submit(function(event){
-  event.preventDefault();
   var userInput = $("input#number").val();
-});
 
   var for3 = "I'm sorry, Dave. I'm afraid I can't do that."
   var for2 = "Boop"
   var for1 = "Beep"
   var ary = [];
+  var spt = [];
 
 
   for( var i = 0; i <= userInput; i++){
@@ -16,7 +15,13 @@ $(document).ready(function(){
   }
   for(var i = 0; i < ary.length; i++){
     if (ary[i].length > 1){
-      var
+    spt = ary[i].split('');
+    }
+    if(spt.indexOf('1') >=0 && spt.indexOf('2') <0 && spt.indexOf('3')){
+      ary.splice(i,1,for1);
     }
   }
+  alert(spt);
+  event.preventDefault();
+});
 });
