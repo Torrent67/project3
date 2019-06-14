c31dac43d32569539398a60d4e0070fbb9fded9e
Marqis Ramos <atair01@yahoo.com>
2019-06-14 11:48:54 -0700

    setting up initial functions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("form#conV").submit(function(event){
+  event.preventDefault();
+
+var for3 = "I'm sorry, Dave. I'm afraid I can't do that."
+var for2 = "Boop"
+var for1 = "Beep"
+
+   for 
